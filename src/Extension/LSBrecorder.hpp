51b3cdef97f02025c1e4d4d71831a68dfb7c393c
LibroWu <731096310@qq.com>
2021-05-25 22:22:13 +0800

    3 channel 3 bit

--- src/Extension/LSBrecorder.hpp
+++ src/Extension/LSBrecorder.hpp
@@ -1,5 +1,5 @@
 //
-// Created by 73109 on 2021/5/25.
+// Created by Libro on 2021/5/25.
 //
 
 #ifndef IMAGE_STEGANOGRAPHY_LSBRECORDER_HPP
@@ -9,20 +9,20 @@
 #include <string>
 #include <iostream>
 
-template<bool encrypt = true, int Channel = 1>
+template<bool encrypt = true, int Channel = 1, int bitNum = 1>
 class LSBrecorder {
 private:
     std::string input, output;
     ln::LBitmap1 image;
-    int X, Y, x, y, k;
+    int X, Y, x, y, k, num;
 public:
     LSBrecorder(const std::string &input, const std::string &output, int X, int Y) : input(input), output(output), X(X),
-                                                                                     Y(Y), x(0), y(0), k(0) {
+                                                                                     Y(Y), x(0), y(0), k(0), num(0) {
         image.ReadBmp(input.c_str());
     }
 
     LSBrecorder(const std::string &input, int X, int Y) : input(input), X(X),
-                                                          Y(Y), x(0), y(0), k(0) {
+                                                          Y(Y), x(0), y(0), k(0), num(0) {
         image.ReadBmp(input.c_str());
     }
 
@@ -36,19 +36,22 @@ public:
             return;
         }
         unsigned char *p1 = image.Pixel(x, y);
-        if (flag) p1[k] |= (unsigned char) 0b00000001;
-        else p1[k] &= (unsigned char) 0b11111110;
-        ++k;
-        if (k == Channel) {
-            k = 0;
-            ++x;
-            if (x == X) {
-                ++y;
-                x = 0;
+        if (flag) p1[k] |= ((unsigned char) 0b00000001) << num;
+        else p1[k] &= (unsigned char) 0b11111111 ^ ((unsigned char) 0b00000001 << num);
+        ++num;
+        if (num == bitNum) {
+            num = 0;
+            ++k;
+            if (k == Channel) {
+                k = 0;
+                ++x;
+                if (x == X) {
+                    ++y;
+                    x = 0;
+                }
             }
         }
     }
-
 
     bool getBit() {
         if (encrypt) {
@@ -60,14 +63,18 @@ public:
             return 0;
         }
         unsigned char *p = image.Pixel(x, y);
-        bool res = p[k] & (unsigned char) 1;
-        ++k;
-        if (k == Channel) {
-            k = 0;
-            ++x;
-            if (x == X) {
-                ++y;
-                x = 0;
+        bool res = p[k] & ((unsigned char) (1 << num));
+        ++num;
+        if (num == bitNum) {
+            num = 0;
+            ++k;
+            if (k == Channel) {
+                k = 0;
+                ++x;
+                if (x == X) {
+                    ++y;
+                    x = 0;
+                }
             }
         }
         return res;
---
+++ src/Extension/city_encrypt.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Libro on 2021/5/25.
+//
+
+#include "LSBrecorder.hpp"
+
+int main(int argc, char **argv) {
+    LSBrecorder<true, 3,3> recorder("sky-s.bmp", "sky-modified.bmp", 1024, 768);
+    ln::LBitmap1 image1;
+    image1.ReadBmp("city.bmp");
+    for (size_t y = 0; y < 768; ++y) {
+        for (size_t x = 0; x < 1024; ++x) {
+            unsigned char *p1 = image1.Pixel(x, y);
+            for (int i = 0; i < 8; ++i)
+                recorder.writeBit(p1[0] & ((unsigned char)1<<i));
+        }
+    }
+    return 0;
+}
+
